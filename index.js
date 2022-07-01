f7b7d111603206fc656c5bdf6946b35fee17cf57
Nguyễn Thanh Nguyên <nguyenkiddo0@gmail.com>
2022-07-01 19:13:33 +0700

    Rename route to 'trang-chu'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/tin-tuc", (req, res) => {
+app.get("/trang-chu", (req, res) => {
   var a = 1;
   var b = 2;
 
